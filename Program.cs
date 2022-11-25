217b29b12846b004bb3d9f1fba9565cc7a7456fc
MahmoudNegm1 <77124033+MahmoudNegm1@users.noreply.github.com>
2022-11-25 10:28:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine( " enter your first number "  );
+ decimal firstNumber = decimal.Parse( Console.ReadLine() );
+Console.WriteLine(" enter your second number ");
+decimal secondNumber = decimal.Parse(Console.ReadLine());
+Console.WriteLine($" the sum = { firstNumber+secondNumber} \n multiplication = { firstNumber*secondNumber} \n " +
+    $"division =  {firstNumber/ secondNumber} \n modulas = { firstNumber%secondNumber} ");
+
+
+
